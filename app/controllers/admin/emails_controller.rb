a3ae90d83bb97725de836e41503280a0c4d5cb90
Adi Purnama <adiprnm2014@gmail.com>
2025-06-17 23:12:36 +0700

    Verbose error

--- app/controllers/admin/emails_controller.rb
+++ app/controllers/admin/emails_controller.rb
@@ -1,10 +1,10 @@
 class Admin::EmailsController < AdminController
   def test
     DummyMailer.test_email(params[:email_address]).deliver_now
-    message = "Email berhasil dikirim! Silahkan cek kotak masuk."
-  rescue
-    message = "Email gagal dikirim. Silahkan cek kembali alamat atau pengaturan email."
+    message = { notice: "Email berhasil dikirim! Silahkan cek kotak masuk." }
+  rescue => e
+    message = { alert: "Email gagal dikirim. Silahkan cek kembali alamat atau pengaturan email. Pesan: #{ e.message }" }
   ensure
-    redirect_to admin_emails_path, notice: message
+    redirect_to admin_emails_path, **message
   end
 end
