6692cb43f3098ab6140ca6d6a560f2144bfa135b
Adi Purnama <adiprnm2014@gmail.com>
2025-06-19 09:14:57 +0700

    Use hashed IP address as the cart identifier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,12 @@ class ApplicationController < ActionController::Base
     end
 
     def set_current_cart
-      session[:cart_session_id] ||= SecureRandom.uuid
+      session[:cart_session_id] ||= cart_session_id
       Current.cart = Cart.find_or_create_by(session_id: session[:cart_session_id])
+    end
+
+    def cart_session_id
+      ip_address = request.headers["CF-Connecting-IP"].presence || request.remote_ip
+      Digest::SHA256.hexdigest(ip_address)
     end
 end
