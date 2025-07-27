44436f8eadb471673b32e127d3c354131554e9a1
Adi Purnama <adiprnm2014@gmail.com>
2025-07-28 02:26:35 +0700

    Add product sold count

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,9 @@ class Product < ApplicationRecord
   has_one_attached :featured_image
   has_and_belongs_to_many :categories
 
+  has_many :order_line_items, as: :orderable
+  has_many :completed_orders, -> { paid }, through: :order_line_items, source: :order
+
   enum :state, %w[ inactive active ]
 
   def self.create_with_productable(product_params, productable_params)
