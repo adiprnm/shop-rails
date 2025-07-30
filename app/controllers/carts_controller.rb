c69931f444f60dbb31ce3c3d04ce111be109fb21
Adi Purnama <adiprnm2014@gmail.com>
2025-07-30 16:47:00 +0700

    Logic for showing the message in cart page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,8 @@
 class CartsController < ApplicationController
+  def show
+    if params[:order_id].present?
+      @order = Order.find_or_initialize_by(order_id: params[:order_id])
+      @show_message = @order.paid?
+    end
+  end
 end
