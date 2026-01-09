dbe7e21e34be0e5a9d8258673fc0bff89b734367
Adi Purnama <adiprnm2014@gmail.com>
2026-01-10 00:31:18 +0700

    Add tests for physical product flow (shop-7zt)
    - Add unit tests for PhysicalProduct model
    - Add unit tests for ProductVariant model
    - Fix config/cache.yml YAML syntax issue
    - Add test for at_least_one_active_variant validation
    - Add tests for scopes (active, in_stock)
    - All 220 tests passing

--- test/models/physical_product_test.rb
+++ test/models/physical_product_test.rb
@@ -1,7 +1,46 @@
 require "test_helper"
 
 class PhysicalProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @physical_product = PhysicalProduct.new
+  end
+
+  test "should have many product_variants" do
+    assert_respond_to @physical_product, :product_variants
+  end
+
+  test "should have one product" do
+    assert_respond_to @physical_product, :product
+  end
+
+  test "should accept weight" do
+    @physical_product.weight = 1000
+    @physical_product.requires_shipping = true
+    assert @physical_product.valid?
+  end
+
+  test "should accept requires_shipping" do
+    @physical_product.weight = 1000
+    @physical_product.requires_shipping = true
+    assert @physical_product.valid?
+  end
+
+  test "should not accept negative weight" do
+    @physical_product.weight = -100
+    @physical_product.requires_shipping = true
+    assert_not @physical_product.valid?
+    assert_includes @physical_product.errors[:weight], "must be greater than 0"
+  end
+
+  test "should allow nil weight" do
+    @physical_product.weight = nil
+    @physical_product.requires_shipping = false
+    assert @physical_product.valid?
+  end
+
+  test "should be valid with no variants" do
+    @physical_product.weight = 100
+    @physical_product.requires_shipping = true
+    assert @physical_product.valid?
+  end
 end
