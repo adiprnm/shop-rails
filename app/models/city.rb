5e2dc13712d786076a50757805cee07282a70a66
Adi Purnama <adiprnm2014@gmail.com>
2026-01-12 07:27:21 +0700

    Add default scope

--- app/models/city.rb
+++ app/models/city.rb
@@ -5,6 +5,8 @@ class City < ApplicationRecord
   validates :rajaongkir_id, presence: true, uniqueness: true
   validates :name, presence: true
 
+  default_scope { order(name: :asc) }
+
   def self.find_by_rajaongkir_id(rajaongkir_id)
     find_by(rajaongkir_id: rajaongkir_id)
   end
--- app/models/subdistrict.rb
+++ app/models/subdistrict.rb
@@ -4,6 +4,8 @@ class Subdistrict < ApplicationRecord
   validates :rajaongkir_id, presence: true, uniqueness: true
   validates :name, presence: true
 
+  default_scope { order(name: :asc) }
+
   def self.find_by_rajaongkir_id(rajaongkir_id)
     find_by(rajaongkir_id: rajaongkir_id)
   end
