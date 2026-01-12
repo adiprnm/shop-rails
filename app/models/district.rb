ad0271b62e3514b141822ae385cdd7ca997f028c
Adi Purnama <adiprnm2014@gmail.com>
2026-01-12 09:51:05 +0700

    Use phrased method instead of name

--- app/models/district.rb
+++ app/models/district.rb
@@ -10,4 +10,8 @@ class District < ApplicationRecord
   def self.find_by_rajaongkir_id(rajaongkir_id)
     find_by(rajaongkir_id: rajaongkir_id)
   end
+
+  def phrased
+    "#{ name }, #{ city.name }, #{ city.province.name }"
+  end
 end
