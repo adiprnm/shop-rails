92b5ed9fbeda611e8da7277ea04f641ed58a40d2
Adi Purnama <adiprnm2014@gmail.com>
2026-02-16 12:17:53 +0700

    Fetch the settings if it isn't set yet

--- app/models/current.rb
+++ app/models/current.rb
@@ -8,6 +8,9 @@ class Current < ActiveSupport::CurrentAttributes
   end
 
   def settings
-    super.to_h
+    current_settings = super.to_h
+    return current_settings if current_settings.present?
+
+    Current.settings = Setting.all.with_attached_file.to_a.map { |setting| [ setting.key, setting.value ] }.to_h
   end
 end
