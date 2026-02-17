d7b722de2d9b38aaf5db223bb9537616ffd1c024
Adi Purnama <adiprnm2014@gmail.com>
2026-02-17 23:46:11 +0700

    Change to r2

--- app/models/database_backup.rb
+++ app/models/database_backup.rb
@@ -1,3 +1,3 @@
 class DatabaseBackup < ApplicationRecord
-  has_one_attached :file, service: Rails.env.production? ? :database_backup : :test
+  has_one_attached :file, service: Rails.env.production? ? :r2 : :test
 end
